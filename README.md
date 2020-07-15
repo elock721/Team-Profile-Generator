78c8796dd0d5d6f71c16227d6b61a1e6bea43d1a
Elliott Lockwood <lockwood721@gmail.com>
2020-07-14 22:59:31 -0500

    Final code and readme draft

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Team-Profile-Generator Overview
 
-This team profile generator CLI allows the user to efficiently generete the profiles of all involved team members
+This team profile generator CLI allows the user to efficiently generate the profiles of all involved team members
 
 The user will first open up the app.js folder in the terminal and then run "node app.js"
 
@@ -13,6 +13,39 @@ When the user is done entering all necessary employees, all the entered data wil
 ---------------------------------------------
 
 # Psuedo Code
+
+The user will first open up the terminal at the app.js folder and run "node app.js"
+
+The askQuestions function will then prompt them with the first 3 initial questions for all employes from the questionBank array
+
+The 4th and last question in the questionBank array will identify what type of employee the user is entering and the following switch statement will prompt the proper follow up question for that position from the followUpQuestions array
+
+The continueAnswer let variable below that will allow the user to keep adding as many employees as they would like
+
+Once the user is finished adding all the employees they would like, the writeOutput function is called and organizes/renders all the entered data in the team HTML file with the help of the htmlRenderer JS file
+
+---------------------------------------------
+
+NPM Package: Inquierer 
+
+The project consists of: 
+
+4 JS files containing the different classes - Employee, Engineer, Intern, Manager
+
+4 JS test files - Employee, Engineer, Intern, Manager
+
+1 JS file containing the htmlRenderer code
+
+1 JS file with the code for the application - app.js
+
+1 HTML file where all the entered data is populated - team.html
+
+4 HTML templates - Employee, Engineer, Intern, Manager
+
+3 JSON files - package, package-lock, settings
+
+
+
 
 
 
--- app.js
+++ app.js
@@ -10,33 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-// question that allows user to continue adding employees
 const continueQuestions = {
     type: 'input',
     name: 'continue',
@@ -91,6 +64,16 @@ const followUpQuestions = [
 // empty array where entered employee data is pushed to
 const employees = [];
 
+
+// function to render the HTML file 
+writeOutput =() => {
+    if(!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(employees), "utf-8")
+}
+
+
     // function that produces the questions 
     async function askQuestions() {
         let continueAsking = true;
@@ -123,19 +106,14 @@ const employees = [];
         }
         // asks user if they would like to keep adding employees
         let continueAnswer = await inquirer.prompt(continueQuestions);
-        continueAsking = continueAnswer == 'yes';
+        continueAsking = continueAnswer.continue.toLowerCase() === 'yes';
     }
-    console.log(employees);
+    // rendering to html
+    writeOutput();
 }
 
 // calling of the askQuestions function
 askQuestions();
 
 
-// calling to render the HTML file 
-writeOutput =() => {
-    if(!fs.existsSync(OUTPUT_DIR)) {
-        fs.mkdirSync(OUTPUT_DIR)
-    }
-    fs.writeFileSync(outputPath, render(employees), "utf-8")
-}
+
